2997264c582f84ea2698b6e9f439dc8006734733
Adeilsonnm <adeilson.n.macedo@gmail.com>
2024-07-19 13:15:34 -0300

    ok

---
+++ firebaseConfig.js
@@ -0,0 +1,13 @@
+// Your web app's Firebase configuration
+    // Sua configuração do Firebase
+    export const firebaseConfig = {
+        apiKey: "AIzaSyCRvFbJquX10Ydb79A9IgVv6PuvreAB3YI",
+        // authDomain: "gsite-65539.firebaseapp.com",
+        // authDomain: "localhost",
+        authDomain: "adeilsonnm.github.io",
+        projectId: "gsite-65539",
+        storageBucket: "gsite-65539.appspot.com",
+        messagingSenderId: "32741284154",
+        appId: "1:32741284154:web:626f96c89131cbff919803",
+        measurementId: "G-KW05V6950H"
+};
